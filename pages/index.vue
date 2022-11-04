0a7788b1da8e01e23ba9857dc57375bbd8ad8728
WiseMrMusa <abdulkareemusa01@gmail.com>
2022-11-04 19:40:56 +0100

    List of dresses added

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,17 @@
 <template>
     <div class="background">
         <h1>Fash Vix Marketplace</h1>
+
+        <div>
+            <h3>Fashion Attire</h3>
+
+            <div>
+                <div v-for="attire in attires" :key="attire.id">
+                    <h3>{{attire.name}}</h3>
+                    <p>{{attire.likes}}</p>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -13,3 +24,73 @@ h1 {
     font-size: 2em;
 }
 </style>
+
+<script>
+export default {
+    data() {
+        return {
+            attires: [
+                {
+                    id: 1,
+                    name: "Culotte dress",
+                    likes: 0
+                },
+
+                {
+                    id: 2,
+                    name: "Babydoll dress",
+                    likes: 0
+                },
+                {
+                    id: 3,
+                    name: "Wrap around dress",
+                    likes: 0
+                },
+                {
+                    id: 4,
+                    name: "Kimono dress",
+                    likes: 0
+                },
+                {
+                    id: 5,
+                    name: "Bubble dress",
+                    likes: 0
+                },
+                {
+                    id: 6,
+                    name: "Tutu dress",
+                    likes: 0
+                },
+                {
+                    id: 7,
+                    name: "Polo dress",
+                    likes: 0
+                },
+                {
+                    id: 8,
+                    name: "Agbada Dress",
+                    likes: 0
+                },
+                {
+                    id: 9,
+                    name: "Buba dress",
+                    likes: 0
+                },
+                {
+                    id: 10,
+                    name: "Gele dress",
+                    likes: 0
+                },
+            ]
+        }
+    }
+}
+</script>
+
+
+
+
+
+
+
+
