ffef6fcb398ab98bf5a125f5e23d6a37c924552e
WiseMrMusa <abdulkareemusa01@gmail.com>
2022-11-04 19:24:12 +0100

    I am just starting

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,15 @@
 <template>
-    <div>
-    <slot />
-    <NuxtWelcome />
+    <div class="background">
+        <h1>Fash Vix Marketplace</h1>
     </div>
-</template>`~
+</template>
+
+<style>
+* {
+    font-family: Inter, Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+}
+
+h1 {
+    font-size: 2em;
+}
+</style>
